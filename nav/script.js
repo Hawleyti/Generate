021d2453f7916a2d7bfbca7db98963fe2a053f8e
Marcos947 <zhangting3451@gmail.com>
2024-06-24 13:29:29 -0400

    0624更新

--- nav/script.js
+++ nav/script.js
@@ -12,29 +12,3 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 });
 
-function loadImages(filter) {
-	// 清空图片容器
-	var imageContainer = document.getElementById("imageContainer");
-	imageContainer.innerHTML = "";
-
-	// 加载对应的图片内容
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			imageContainer.innerHTML = this.responseText;
-		}
-	};
-	xhr.open("GET", filter + ".html", true);
-	xhr.send();
-}
-
-function showModal(event) {
-	// 显示模态框
-	// ...
-}
-
-function closeModal() {
-	// 关闭模态框
-	// ...
-}
-
